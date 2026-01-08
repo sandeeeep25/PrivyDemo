dd5ef6df86dc2bca29f01b889096152160ca8c94
sandeeeep25 <107317715+sandeeeep25@users.noreply.github.com>
2026-01-08 11:33:53 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -31,8 +31,8 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
 
     if (response.ok) {
         alert("Login successful!");
-        // if (result && result.userId) localStorage.setItem("userId", result.userId);
-        window.location.href = "index.html";
+        if (result && result.userId) localStorage.setItem("userId", result.userId);
+        window.location.href = "pia.html";
     } else {
         const message = (result && result.message) ? result.message : (typeof result === 'string' ? result : JSON.stringify(result));
         alert("Error: " + message);
